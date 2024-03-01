c444a60057709679f39a670bf303bead5640a764
kendall2217 <kensan129@gmail.com>
2024-02-29 18:12:03 -0600

    Semana 6
    Push de la materia vista en semana 6

--- Tienda_v1_Web/src/main/java/com/tienda/dao/CategoriaDao.java
+++ Tienda_v1_Web/src/main/java/com/tienda/dao/CategoriaDao.java
@@ -4,10 +4,13 @@
  */
 package com.tienda.dao;
 
+import com.tienda.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  *
  * @author kensa
  */
-public interface CategoriaDao {
+public interface CategoriaDao extends JpaRepository <Categoria, Long>{
     
 }
